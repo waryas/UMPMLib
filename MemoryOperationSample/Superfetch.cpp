f777c9eb23cff8995a1222543a66b819b2845309
waryas <waryas.z@gmail.com>
2017-10-22 05:59:30 +0200

    more cleanup + pooltag scanner operational on live memory

--- MemoryOperationSample/Superfetch.cpp
+++ MemoryOperationSample/Superfetch.cpp
@@ -3,78 +3,6 @@
 #include <Windows.h>
 #include <memory>
 
-PCHAR Priorities[8] =
-{
-	"Idle",
-	"Very Low",
-	"Low",
-	"Background",
-	"Background",
-	"Normal",
-	"SuperFetch",
-	"SuperFetch"
-};
-
-PCHAR ShortPfnList[TransitionPage + 1] =
-{
-	"Zero",
-	"Free",
-	"Standby",
-	"Modified",
-	"Mod No-Write",
-	"Bad",
-	"Active",
-	"Transition"
-};
-
-PCHAR UseList[MMPFNUSE_KERNELSTACK + 1] =
-{
-	"Process Private",
-	"Memory Mapped File",
-	"Page File Mapped",
-	"Page Table",
-	"Paged Pool",
-	"Non Paged Pool",
-	"System PTE",
-	"Session Private",
-	"Metafile",
-	"AWE Pages",
-	"Driver Lock Pages",
-	"Kernel Stack"
-};
-
-PCHAR ShortUseList[MMPFNUSE_KERNELSTACK + 1] =
-{
-	"Process Private",
-	"Mem Mapped File",
-	"PF Mapped File",
-	"Page Table",
-	"Paged Pool",
-	"Non Paged Pool",
-	"System PTE",
-	"Session Private",
-	"Metafile",
-	"AWE ",
-	"Driver Locked",
-	"Kernel Stack"
-};
-
-SIZE_T MmPageCounts[TransitionPage + 1];
-SIZE_T MmUseCounts[MMPFNUSE_KERNELSTACK + 1];
-SIZE_T MmPageUseCounts[MMPFNUSE_KERNELSTACK + 1][TransitionPage + 1];
-SIZE_T MmHighestPhysicalPage;
-ULONG MmHighestPhysicalPageNumber;
-PPF_PFN_PRIO_REQUEST MmPfnDatabase;
-RTL_BITMAP MmVaBitmap, MmPfnBitMap;
-PPF_PRIVSOURCE_QUERY_REQUEST MmPrivateSources;
-LIST_ENTRY MmProcessListHead;
-ULONG MmProcessCount;
-LIST_ENTRY MmFileListHead;
-ULONG MmFileCount;
-ULONG MmPfnDatabaseSize;
-HANDLE PfiFileInfoHandle;
-PPF_MEMORY_RANGE_INFO MemoryRanges;
-
 template<typename SYS_TYPE>
 std::unique_ptr<SYS_TYPE>
 QueryInfo(
@@ -104,42 +32,12 @@ QueryInfo(
 	return r_info;
 }
 
-void PfiBuildSuperfetchInfo(IN PSUPERFETCH_INFORMATION SuperfetchInfo, IN PVOID Buffer, IN ULONG Length, IN SUPERFETCH_INFORMATION_CLASS InfoClass) {
+inline void SFBuildInfo(IN PSUPERFETCH_INFORMATION SuperfetchInfo, IN PVOID Buffer, IN ULONG Length, IN SUPERFETCH_INFORMATION_CLASS InfoClass) {
 	SuperfetchInfo->Version = SUPERFETCH_VERSION;
 	SuperfetchInfo->Magic = SUPERFETCH_MAGIC;
 	SuperfetchInfo->Data = Buffer;
 	SuperfetchInfo->Length = Length;
 	SuperfetchInfo->InfoClass = InfoClass;
-}
-
-NTSTATUS PfiQueryMemoryRanges() {
-	NTSTATUS Status;
-	SUPERFETCH_INFORMATION SuperfetchInfo;
-
-	ULONG ResultLength = 0;
-
-	PF_MEMORY_RANGE_INFO MemoryRangeInfo;
-	MemoryRangeInfo.Version = 1;
-
-	PfiBuildSuperfetchInfo(&SuperfetchInfo, &MemoryRangeInfo, sizeof(MemoryRangeInfo), SuperfetchMemoryRangesQuery);
-
-	Status = NtQuerySystemInformation(SystemSuperfetchInformation, &SuperfetchInfo, sizeof(SuperfetchInfo), &ResultLength);
-	if (Status == STATUS_BUFFER_TOO_SMALL) {
-
-		MemoryRanges = static_cast<PPF_MEMORY_RANGE_INFO>(HeapAlloc(GetProcessHeap(), 0, ResultLength));
-		MemoryRanges->Version = 1;
-
-		PfiBuildSuperfetchInfo(&SuperfetchInfo, MemoryRanges, ResultLength, SuperfetchMemoryRangesQuery);
-
-		Status = NtQuerySystemInformation(SystemSuperfetchInformation, &SuperfetchInfo, sizeof(SuperfetchInfo), &ResultLength);
-		if (!NT_SUCCESS(Status))
-			return Status;
-	}
-	else {
-		MemoryRanges = &MemoryRangeInfo;
-	}
-
-	return STATUS_SUCCESS;
 }
 
 bool SFSetup()
@@ -157,15 +55,32 @@ bool SFSetup()
 	if (!NT_SUCCESS(status)) 
 		return false;
 
-	MmHighestPhysicalPageNumber = basicInfo.HighestPhysicalPageNumber;
 
 	return true;
 }
 
 bool SFGetMemoryInfo(SFMemoryInfo* pInfo, int& rCount)
 {
-	if (!NT_SUCCESS(PfiQueryMemoryRanges()))
-		return false;
+	PPF_MEMORY_RANGE_INFO MemoryRanges;	
+	SUPERFETCH_INFORMATION SuperfetchInfo;
+	ULONG ResultLength = 0;
+	PF_MEMORY_RANGE_INFO MemoryRangeInfo;
+	MemoryRangeInfo.Version = 1;
+	SFBuildInfo(&SuperfetchInfo, &MemoryRangeInfo, sizeof(MemoryRangeInfo), SuperfetchMemoryRangesQuery);
+
+	if (
+		NtQuerySystemInformation(SystemSuperfetchInformation, &SuperfetchInfo, sizeof(SuperfetchInfo), &ResultLength) 
+		== STATUS_BUFFER_TOO_SMALL) 
+	{
+		MemoryRanges = static_cast<PPF_MEMORY_RANGE_INFO>(HeapAlloc(GetProcessHeap(), 0, ResultLength));
+		MemoryRanges->Version = 1;
+		SFBuildInfo(&SuperfetchInfo, MemoryRanges, ResultLength, SuperfetchMemoryRangesQuery);
+		if (!NT_SUCCESS(NtQuerySystemInformation(SystemSuperfetchInformation, &SuperfetchInfo, sizeof(SuperfetchInfo), &ResultLength)))
+			return false;
+	}
+	else {
+		MemoryRanges = &MemoryRangeInfo;
+	}
 
 	rCount = 0;
 	PPHYSICAL_MEMORY_RUN Node;
@@ -204,7 +119,7 @@ uint64_t SFGetModuleBase(char* module)
 	auto module_info = QueryInfo<RTL_PROCESS_MODULES>(SystemModuleInformation);
 
 	for (size_t i = 0; i < module_info->NumberOfModules; i++)
-		if (!strnicmp(module, module_info.get()->Modules[i].FullPathName + module_info->Modules[i].OffsetToFileName, strlen(module)+1))
+		if (!_strnicmp(module, module_info.get()->Modules[i].FullPathName + module_info->Modules[i].OffsetToFileName, strlen(module)+1))
 			return reinterpret_cast<size_t>(module_info->Modules[i].ImageBase);
 
 	return 0;
